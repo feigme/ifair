a69611b38e82897e0a9f7957617a7cd031cc97cf
feiying <feiying@huigujia.cn>
2016-12-21 15:30:20 +0800

    oauth2

--- ifair-web/src/main/java/com/ifair/web/index/controller/IndexController.java
+++ ifair-web/src/main/java/com/ifair/web/index/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.ifair.web.index.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,16 +12,15 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 
 	@Value("${oauth2.domain}")
-	protected String oauth2Domain;
-
-	@ModelAttribute("oauth2Domain")
-	public String oauth2Domain() {
-		return oauth2Domain;
-	}
+	private String oauth2Domain;
+	@Value("${oauth2.client.id}")
+	private String oauth2ClientId;
 
 	@RequestMapping("/index")
 	public ModelAndView index() {
-		return new ModelAndView("views/index");
+		ModelAndView mav = new ModelAndView("views/index");
+		mav.getModel().put("loginUrl", String.format("%s/oauth2/authorize?client_id=%s&response_type=%s&redirect_uri=%s", oauth2Domain, oauth2ClientId, "code", "http://www.baidu.com"));
+		return mav;
 	}
 
 }

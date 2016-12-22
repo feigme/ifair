d0a1cca9b07c8439624be8817b5f971b6cef7ead
feiying <feiying@huigujia.cn>
2016-12-22 10:17:29 +0800

    修改获取资源

--- ifair-web/src/main/java/com/ifair/web/user/controller/UserController.java
+++ ifair-web/src/main/java/com/ifair/web/user/controller/UserController.java
@@ -16,7 +16,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
